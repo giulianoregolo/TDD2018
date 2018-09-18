121253228354fb278afb790c2c2bd0e414ef1003
giulianoregolo <41971577+giulianoregolo@users.noreply.github.com>
2018-09-18 17:03:19 -0300

    agrego la funcion para ver la CantCartas
    agrego solo el test

--- tests/MazoTest.php
+++ tests/MazoTest.php
@@ -10,14 +10,19 @@ class MazoTest extends TestCase {
      * Valida que se puedan crear mazos de cartas.
      */
     public function testExiste() {
-        $mazo = new Mazo;
+        $mazo = new Mazo($CantidadCartas);
         $this->assertTrue(isset($mazo));
     }
 
     public function testMezclable() {
-        $mazo = new Mazo;
+        $mazo = new Mazo($CantidadCartas);
         $this->assertTrue($mazo->mezclar());
     }
-
+    
+    public function testCantcartas(){
+        $CantidadCartas = 40;
+        $mazo = new Mazo($CantidadCartas);
+        $this->assertEquals($mazo->obtenerCantidadCartas(),40);
+    }
 
 }
