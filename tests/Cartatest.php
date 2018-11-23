df6eff690a39ce160e250953cca25e31f0abd60d
giulianoregolo <41971577+giulianoregolo@users.noreply.github.com>
2018-11-23 15:37:59 -0300

    arreglo en los test

--- tests/Cartatest.php
+++ tests/Cartatest.php
@@ -7,19 +7,16 @@ use PHPUnit\Framework\TestCase;
 class Cartatest extends TestCase{
     public function testcartaTipo(){
         $carta = new Carta("rojo",5,"poker");
-        $tipo = $carta->obtenertipo();
-        $this->assertEquals($tipo,"poker"); 
+        $this->assertEquals($carta->obtenertipo(),"poker"); 
        
-    }
-    public function testcartaNumero(){
-        $carta = new Carta("rojo",5,"poker");
-        $palo = $carta->obtenerpalo();
-        $this->assertEquals($palo,"rojo");
-        
     }
     public function testcartaPalo(){
         $carta = new Carta("rojo",5,"poker");
-        $numero = $carta->obtenernumero();
-        $this->assertEquals($numero,5);
+        $this->assertEquals($carta->obtenerpalo(),"rojo");
+        
+    }
+    public function testcartaNumero(){
+        $carta = new Carta("rojo",5,"poker");
+        $this->assertEquals($carta->obtenernumero(),5);
     }
 }
