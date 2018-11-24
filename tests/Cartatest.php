4c3ed33f85d932367dbbf99221a96edeb96d0f7f
Giuliano Regolo <gripdesexto6e@gmail.com>
2018-11-23 22:51:31 -0300

    arreglo en el test carta

--- tests/Cartatest.php
+++ tests/Cartatest.php
@@ -7,19 +7,19 @@ use PHPUnit\Framework\TestCase;
 class Cartatest extends TestCase{
     public function testcartaTipo(){
         $carta = new Carta("rojo",5,"poker");
-        $tipo = $this->obtenertipo();
+        $tipo = $carta->obtenertipo();
         $this->assertEquals($tipo,"poker"); 
        
     }
     public function testcartaNumero(){
         $carta = new Carta("rojo",5,"poker");
-        $palo = $this->obtenerpalo();
+        $palo = $carta->obtenerpalo();
         $this->assertEquals($palo,"rojo");
         
     }
     public function testcartaPalo(){
         $carta = new Carta("rojo",5,"poker");
-        $numero = $this->obtenernumero();
+        $numero = $carta->obtenernumero();
         $this->assertEquals($numero,5);
     }
 }
